a9ae24737c6f8970aa0130e95034a9c0f068dae6
Teresa Fontanella De Santis <tere.fds@hotmail.com>
2016-05-18 22:49:28 -0300

    putting updates on file

--- hello.py
+++ hello.py
@@ -4,7 +4,6 @@ from flask import Response
 from flask import abort
 
 app = Flask(__name__)
-updates = ['1','2','3']
 
 @app.route('/')
 def hello_world():
@@ -15,23 +14,16 @@ def facebook_data():
   if request.method == 'GET':
   	if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == 'token':
   	  resp = Response(request.args.get('hub.challenge'), status=200)
+  	  with open("test.txt", "a") as myfile:
+  		myfile.write('get_facebook_93'+'\n')
   	else:
   	  abort(400)
   	return resp
   elif request.method == 'POST':
-  	updates= updates + request.get_data()
   	with open("test.txt", "a") as myfile:
+  		myfile.write('post_facebook_93'+'\n')
   		myfile.write(str(request.get_data())+'\n')
-  	return 200
-
-@app.route('/facebook/list', methods=['GET'])
-def facebook_list():
-  #str1 = " ".join(str(x) for x in updates)
-  str1 = " "
-  with open("test.txt", "a") as myfile:  
-    for ups in updates:
-  	  myfile.write(str(ups)+'\n')
-  return str1
+  	return '200'
  
 
 
